1b60de6be3574c9eb103ff88a085f61e413b423b
Peggy Chen <peggychen@Peggys-MacBook-Pro.local>
2025-09-22 20:17:31 +0800

    Updates

---
+++ precache-manifest.583eb5dc2db12571ce0a68226ef01a5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e21889d0c2966f7aaf169577f24b2f5",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "18d92d861a8c17366290",
+    "url": "/portfolio/static/css/main.b02d7664.chunk.css"
+  },
+  {
+    "revision": "4fbe3e9ea12b613b61fa",
+    "url": "/portfolio/static/js/2.5195f722.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/portfolio/static/js/2.5195f722.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18d92d861a8c17366290",
+    "url": "/portfolio/static/js/main.f9987709.chunk.js"
+  },
+  {
+    "revision": "13fa3c2584c758b03d4c",
+    "url": "/portfolio/static/js/runtime-main.38fa006e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.a2952560469b025a66ba2689ac9c39c3.js"
+  "/portfolio/precache-manifest.583eb5dc2db12571ce0a68226ef01a5f.js"
 );
 
 self.addEventListener('message', (event) => {
